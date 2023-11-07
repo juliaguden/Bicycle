161fdad419828bd3878feb8e348b5361fc4d56c8
juliaguden <julia_guden25@milton.edu>
2023-11-07 08:51:38 -0500

    2nd

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+    public static void main(String[] args) {
+
+        Rectangle A = new Rectangle(20,10);
+        A.display();
+
+        A.getArea();
+        A.getPerimeter();
+
+
+    }
+}
