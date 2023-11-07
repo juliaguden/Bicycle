9b411b5bc5672ec00c1b0a3ff2c4143b3baaedf6
juliaguden <julia_guden25@milton.edu>
2023-11-07 08:51:02 -0500

    2nd

---
+++ src/Rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle {
+    public int length;
+    public int width;
+
+    public Rectangle (int plength, int pwidth){
+        length=plength;
+        width=pwidth;
+    }
+    public void display(){
+        System.out.println("length "+ length + " width "+ width);
+    }
+    public void getArea(){
+        System.out.println(length * width);
+    }
+    public void getPerimeter(){
+        System.out.println(length+length+width+width);
+    }
+}
